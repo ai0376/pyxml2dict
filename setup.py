d3006a55f7fe9711a19dc6670359a964eb621999
mjrao <mjrao520@gmail.com>
2017-08-08 20:55:42 +0800

    a

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(
     name='pyxml2dict',
-    version='0.1',
+    version='0.0.1',
     author='mjrao',
     author_email='mjrao520@gmail.com',
     description='Convert XML to python Dict',
